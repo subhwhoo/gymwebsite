de761268ccdc0028d80e427e649731e58abb6d72
Subham Chowdhury <itz_subhamrock@outlook.com>
2023-02-27 00:18:06 +0530

    Not Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar.jsx";
 
 function App() {
   return (
     <div>
-      <h1 className={""}>Subham</h1>
+      <h1>Subham</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <h1 className={"text-black font-bold text-2xl"}>Navbar </h1>
+      <h2>Subham Chowdhury</h2>
+    </div>
+  );
+}
+
+export default Navbar;
